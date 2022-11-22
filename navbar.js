fc4764576ffc68c45c6574c7296d3e218e600fd2
Abhishek choudhary <110372083+abhishekchoudhary0272@users.noreply.github.com>
2022-11-22 23:45:06 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,9 @@
+function activelogin(){
+        document.querySelector(".login").classList.toggle("activelogin");
+        document.querySelector(".images").classList.toggle("container3blur");
+        document.querySelector(".container2").classList.toggle("container3blur");
+        document.querySelector(".container1").classList.toggle("container3blur");
+}
+function myFunction(x) {
+        x.classList.toggle("change");
+      }
