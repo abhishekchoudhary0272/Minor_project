a5461cc718f8f0beca6a24453d08d208f1aeb6c6
Abhishek choudhary <110372083+abhishekchoudhary0272@users.noreply.github.com>
2022-11-23 23:06:31 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -3,6 +3,9 @@ function activelogin(){
         document.querySelector(".images").classList.toggle("container3blur");
         document.querySelector(".container2").classList.toggle("container3blur");
         document.querySelector(".container1").classList.toggle("container3blur");
+        document.querySelector(".flip-card1").classList.toggle("container3blur");
+        document.querySelector(".flip-card2").classList.toggle("container3blur");
+
 }
 function myFunction(x) {
         x.classList.toggle("change");
