941d5e4a3faf48e59fadb60cd06446118251e86c
JatinKumarKushwaha <jatkush222@gmail.com>
2023-01-19 22:13:55 +0530

    cleaned up somethings

--- WEB-INF/classes/com/agrify/servlets/Login.java
+++ WEB-INF/classes/com/agrify/servlets/Login.java
@@ -10,6 +10,7 @@ public class Login extends HttpServlet {
 			String password = request.getParameter("password");
 			System.out.println("email : " + email);
 			System.out.println("password : " + password);
+			
 			RequestDispatcher rd = request.getRequestDispatcher("/buyer_page.html");
 			rd.forward(request, response);
 		} catch (Exception e) {
