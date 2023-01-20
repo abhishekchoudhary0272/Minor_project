0bc6c72d2e9a33cf11382768520e318d2e412812
JatinKumarKushwaha <jatkush222@gmail.com>
2023-01-20 15:05:30 +0530

    added other form elements to dl entities

--- WEB-INF/classes/com/agrify/dl/buyer/BuyerDTO.java
+++ WEB-INF/classes/com/agrify/dl/buyer/BuyerDTO.java
@@ -7,8 +7,11 @@ public class BuyerDTO {
 
 	private int id;
 	private String name;
+	private String userName;
 	private String birth;
 	private String password;
+	private String email;
+	private String phone_number;
 	private String aadhaar_id;
 
 	public BuyerDTO() {
@@ -33,8 +36,20 @@ public class BuyerDTO {
 		this.name = name;
 	}
 
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
 	}
 
 	public void setAadhaar_id(String aadhaar_id) {
@@ -53,11 +68,24 @@ public class BuyerDTO {
 		return name;
 	}
 
+	public String getUserName() {
+		return userName;
+	}
+
 	public String getPassword() {
 		return password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone_number() {
+		return phone_number;
 	}
 
 	public String getAadhaar_id() {
 		return aadhaar_id;
 	}
+
 }
