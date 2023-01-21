5e92793decad0a900ee1533962b6fb254c0e4541
Chinmay Bhide <98650300+illusionnn@users.noreply.github.com>
2023-01-21 12:58:28 +0530

    updated

--- WEB-INF/classes/com/agrify/servlets/Registration.java
+++ WEB-INF/classes/com/agrify/servlets/Registration.java
@@ -1,9 +1,17 @@
 package com.agrify.servlets;
-
+import com.agrify.dl.*;
+import com.agrify.dl.buyer.*;
+import com.agrify.dl.seller.*;
 import java.io.*;
+import java.text.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * r
@@ -11,25 +19,29 @@ import javax.servlet.http.*;
 public class Registration extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) {
 		try {
-			// varibales
+			//varibales 
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			String fName = request.getParameter("fName");
-			String userName = request.getParameter("uName");
+			String lName = request.getParameter("lName");
 			String email = request.getParameter("email");
 			String pNo = request.getParameter("number");
 			String password = request.getParameter("password");
+			String govtNum = request.getParameter("govtNum");
 			String cnfPassword = request.getParameter("cnfPassword");
+			java.util.Date dateOfBirth = simpleDateFormat.parse(request.getParameter("dateOfBirth"));
 			String gender = request.getParameter("gender");
 			String buyerSeller = request.getParameter("BuyerSeller");
 
 			System.out.println("Full name = " + fName);
-			System.out.println("User name = " + userName);
+			System.out.println("Last name = " + lName);
 			System.out.println("Email = " + email);
-			System.out.println("Phone number = " + pNo);
+			System.out.println("Phone number = "+pNo);
 			System.out.println("Password = " + password);
 			System.out.println("Confirm password = " + cnfPassword);
-			System.out.println("Gender = " + gender);
-			System.out.println("BuyerSeller = " + buyerSeller);
-
+			System.out.println("Date of birth = "+dateOfBirth);
+			System.out.println("Gender = "+ gender);
+			System.out.println("BuyerSeller = "+ buyerSeller);
+			
 			PrintWriter pw;
 			pw = response.getWriter();
 			response.setContentType("text/html");
@@ -39,12 +51,35 @@ public class Registration extends HttpServlet {
 			pw.println("<meta charset='utf-8'>");
 			pw.println("<title>something</title>");
 			pw.println("<script>");
-			if (buyerSeller.equals("seller") == true) {
-				pw.println("location.href = \"/Agrify/s\"");
-			} else {
-				pw.println("location.href = \"/Agrify/b\"");
+			System.out.println("hahahaha");
+			if(buyerSeller.equals("buyer")==true){
+				BuyerDTO buyer = new BuyerDTO();
+				buyer.setId(237);
+				buyer.setFirst_name(fName);
+				buyer.setLast_name(lName);
+				buyer.setPassword(password);
+				buyer.setEmail(email);
+				buyer.setPhone_number(pNo);
+				buyer.setBirth(dateOfBirth);
+				buyer.setAadhaar_id(govtNum);
+				BuyerDAOImpl buyerDAO = new BuyerDAOImpl();
+				buyerDAO.insertBuyer(buyer);
+				pw.println("location.href = \"/Agrify/buyer\"");
 			}
-
+			else{
+				SellerDTO seller = new SellerDTO();
+				seller.setId(457);
+				seller.setFirst_name(fName);
+				seller.setLast_name(lName);
+				seller.setBirth(dateOfBirth);
+				seller.setPassword(password);
+				seller.setEmail(email);
+				seller.setPhone_number(pNo);
+				seller.setAadhaar_id(govtNum);
+				SellerDAOImpl sellerDAO = new SellerDAOImpl();
+				sellerDAO.insertSeller(seller);
+				pw.println("location.href = \"/Agrify/seller\"");
+			}		
 			pw.println("</script>");
 			pw.println("</head>");
 			pw.println("<body>");
@@ -53,16 +88,15 @@ public class Registration extends HttpServlet {
 			pw.println("</html>");
 		} catch (Exception e) {
 			System.out.println(e);
-			try {
+			try{
 				RequestDispatcher rd = request.getRequestDispatcher("/index.html");
 				rd.forward(request, response);
-			} catch (Exception se) {
+			}catch(Exception se){
 				System.out.println(se);
-			}
+			}	
 		}
 	}
-
-	public void doPost(HttpServletRequest request, HttpServletResponse response) {
-		doGet(request, response);
+	public void doPost(HttpServletRequest request, HttpServletResponse response){
+		doGet(request,response);
 	}
 }
