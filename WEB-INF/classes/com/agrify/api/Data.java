2490ed03ee0eae59082ead2f212e60b2d1d518e7
JatinKumarKushwaha <jatkush222@gmail.com>
2023-01-31 01:46:53 +0530

    added json support to api with json-simple

--- WEB-INF/classes/com/agrify/api/Data.java
+++ WEB-INF/classes/com/agrify/api/Data.java
@@ -2,20 +2,32 @@ package com.agrify.api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import java.util.HashMap;  
+import java.util.Map;
+import org.json.simple.JSONObject;
 
 /**
  * Data
  */
 
-@Path("/hello")
+@Path("/data")
 public class Data {
 
+	public static Map<String, Object> data = new HashMap<String,Object>();
+
+	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String sayPlainTextHello() {
-		return "Test";
+	@Path("/{email}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response giveData(@PathParam("email") String email) {
+		data.put("email", email);
+		final JSONObject json_string = new JSONObject(data);
+		return Response.status(200).entity(json_string).build();
 	}
 
 }
