0c0094826e9c6ed00ceafef5d64b42d5bececb37
JatinKumarKushwaha <jatkush222@gmail.com>
2023-04-12 15:51:43 +0530

    added url fix in js

--- js/seller_profile.js
+++ js/seller_profile.js
@@ -1,4 +1,26 @@
-api_url = "http://localhost:8080/Agrify/api/data/ram@examplemail.com";
+function parseURLParams(url) {
+	var queryStart = url.indexOf("?") + 1,
+		queryEnd = url.indexOf("#") + 1 || url.length + 1,
+		query = url.slice(queryStart, queryEnd - 1),
+		pairs = query.replace(/\+/g, " ").split("&"),
+		parms = {}, i, n, v, nv;
+
+	if (query === url || query === "") return;
+
+	for (i = 0; i < pairs.length; i++) {
+		nv = pairs[i].split("=", 2);
+		n = decodeURIComponent(nv[0]);
+		v = decodeURIComponent(nv[1]);
+
+		if (!parms.hasOwnProperty(n)) parms[n] = [];
+		parms[n].push(nv.length === 2 ? v : null);
+	}
+	return parms;
+}
+
+const data_new = parseURLParams(location.href);
+
+api_url = "http://localhost:8080/Agrify/api/data/" + data_new.email;
 
 fetch(api_url)
 	.then((response) => {
