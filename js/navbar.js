61a40ceea9ff4220972b0b4cf206b52923938a3e
JatinKumarKushwaha <jatkush222@gmail.com>
2023-04-22 20:16:43 +0530

    made login better

--- js/navbar.js
+++ js/navbar.js
@@ -10,6 +10,18 @@ function activelogin() {
 
 }
 
+function close_login() {
+	document.querySelector(".login").classList.toggle("activelogin");
+	document.querySelector(".images").classList.toggle("container3blur");
+	document.querySelector(".container2").classList.toggle("container3blur");
+	document.querySelector(".container1").classList.toggle("container3blur");
+	document.querySelector(".container3").classList.toggle("container3blur");
+	document.querySelector(".container4").classList.toggle("container3blur");
+	document.querySelector(".container5").classList.toggle("container3blur");
+	document.querySelector(".container6").classList.toggle("container3blur");
+
+}
+
 function myFunction(x) {
 	x.classList.toggle("change");
 	var y = document.querySelector(".sidebar");
